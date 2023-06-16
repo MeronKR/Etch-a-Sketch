84cc5e141dab06f1ace720527ec8d032cbfa65c3
MeronKR <134378150+MeronKR@users.noreply.github.com>
2023-06-16 12:18:17 +0900

    add grid prompt + reset functionality

--- apps.js
+++ apps.js
@@ -1,18 +1,46 @@
-//grid creation
+//grid creation + hover
+let gridSize=0;
+let numSquares = 0;
 
 const body = document.getElementById("container");
+const container =document.getElementById("gridFrame");
+const square = document.createElement("div");
 body.style.display = "flex";
 body.style.justifyContent= "center";
 body.style.alignItems = "center"
 body.style.flexDirection = "column"
 
-const container =document.getElementById("gridFrame");
-container.style.gridTemplateColumns = "repeat(16, 1fr)"
-container.style.gridTemplateRows = "repeat(16, 1fr)"
-container.style.border = "1px solid black";
+const startBtn = document.querySelector(".start");
+startBtn.addEventListener("click", function(){
+    clearGrid();
+    gridSize = parseInt(prompt("How many squares do you want per side from 2-100?"))
+    while (gridSize>100 || gridSize<2){
+        gridSize =  parseInt(prompt("How many squares do you want per side from 2-100?"))
+    }
+    numSquares = gridSize*gridSize
+    createGrid()
+})
 
-for (let i=0; i<256; i++){
-    let div = document.createElement("div");
-    div.style.backgroundColor = "white";
-    container.insertAdjacentElement("beforeend", div)
+function createGrid(){
+    const container =document.getElementById("gridFrame");
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows =  `repeat(${gridSize}, 1fr)`;
+    container.style.border = "1px solid black";
+    
+    for (let i=0; i<numSquares; i++){
+        const square = document.createElement("div");
+        square.style.border = "1px dashed black";
+        square.addEventListener("mouseover", function(){
+            square.style.backgroundColor = "black"
+        } )
+        container.insertAdjacentElement("beforeend", square)
+    };
+}
+
+
+//grid clear
+function clearGrid(){
+    const container =document.getElementById("gridFrame");
+    let squares = container.querySelectorAll("div")
+    squares.forEach((div) => div.remove());
 };
